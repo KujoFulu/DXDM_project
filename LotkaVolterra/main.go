9ac3b3e84c69ad47a6e2313316bfd8b9fcce9ad3
KujoFulu <kujofulu@gmail.com>
2023-11-17 12:08:39 -0500

    Updated several datasets and ini function

--- LotkaVolterra/main.go
+++ LotkaVolterra/main.go
@@ -12,16 +12,39 @@ func main() {
 	// initialize number of species, interaction matrix, and deathGrowth matrix
 	numSpecies := 3
 
+	// population matrix
+	// // 1. orginal paper
+	// pop := []float64{50.0, 10.0, 5.0}
+
+	// 2. stable equilibrium & limit cycle
+	pop := []float64{0.1, 0.8, 0.3}
+
 	// // method 1: randomly generate
 	// interaction := InitializeInteractionMatrix(numSpecies)
 	// method 2: set a matrix by hand
-	interactionSlice := []float64{0, -0.04, -0.04, 0.04, 0, -0.02, 0.02, 0.04, 0}
+
+	// // 1. original paper parameters
+	// interactionSlice := []float64{0, -0.04, -0.04, 0.04, 0, -0.02, 0.02, 0.04, 0}
+
+	// // 2. stable equilibrium
+	// interactionSlice := []float64{-2, -1, 0, 0, -1, -2, -2.6, -1.6, -3}
+
+	// 3. limit cycle
+	interactionSlice := []float64{-0.5, -1, 0, 0, -1, -2, -2.6, -1.6, -3}
+
 	interaction := SetInteractionMatrix(interactionSlice, numSpecies)
 
 	// // method 1: randomly generate
 	// deathGrowth := IniRateMatrix(numSpecies)
+
 	// method 2: set a matrix by hand
-	rateSlice := []float64{0.25, -0.5, -0.5}
+
+	// // 1. original paper parameters
+	// rateSlice := []float64{0.25, -0.5, -0.5}
+
+	// 2. stable equilibrium & limit cycle
+	rateSlice := []float64{3, 4, 7.2}
+
 	deathGrowth := SetRateMatrix(rateSlice)
 
 	// initialize number of generations and time interval
@@ -31,7 +54,7 @@ func main() {
 	fmt.Println("parameters read! Initilizing ecosystem...")
 
 	// initialize an Ecosystem object
-	initialEcosystem := InitializeEcosystem(numSpecies, interaction, deathGrowth)
+	initialEcosystem := InitializeEcosystem(numSpecies, pop, interaction, deathGrowth)
 
 	fmt.Println("Ecosystem initialized! Simulating ecosystem...")
 
@@ -44,7 +67,7 @@ func main() {
 
 	// writing data to csv file
 	fmt.Println("Writing data to csv file...")
-	WriteToCSV(timePoints, "output/data.csv")
+	WriteToCSV(timePoints, "output/data_limit_cycle.csv")
 	fmt.Println("Data written to csv file!")
 
 }
