e41ea46ea3c4d873bfe33aa42cac558b8297cb60
KujoFulu <kujofulu@gmail.com>
2023-11-17 16:59:23 -0500

    Updated several new model type with parameters form a paper

--- LotkaVolterra/main.go
+++ LotkaVolterra/main.go
@@ -10,14 +10,17 @@ func main() {
 	fmt.Println("Reading input parameters...")
 
 	// initialize number of species, interaction matrix, and deathGrowth matrix
-	numSpecies := 3
+	numSpecies := 4
 
 	// population matrix
 	// // 1. orginal paper
 	// pop := []float64{50.0, 10.0, 5.0}
 
-	// 2. stable equilibrium & limit cycle
-	pop := []float64{0.1, 0.8, 0.3}
+	// // 2. stable equilibrium & limit cycle & extinction
+	// pop := []float64{0.1, 0.8, 0.3}
+
+	// 3. chaotic dynamics - 4 species
+	pop := []float64{0.1, 0.8, 0.3, 0.5}
 
 	// // method 1: randomly generate
 	// interaction := InitializeInteractionMatrix(numSpecies)
@@ -29,8 +32,17 @@ func main() {
 	// // 2. stable equilibrium
 	// interactionSlice := []float64{-2, -1, 0, 0, -1, -2, -2.6, -1.6, -3}
 
-	// 3. limit cycle
-	interactionSlice := []float64{-0.5, -1, 0, 0, -1, -2, -2.6, -1.6, -3}
+	// // 3. limit cycle
+	// interactionSlice := []float64{-0.5, -1, 0, 0, -1, -2, -2.6, -1.6, -3}
+
+	// // 4. extinction of one species
+	// interactionSlice := []float64{-2, -1, -1, -1, -1, -2, -2.6, -1.6, -3}
+
+	// // 5. extinction of two species
+	// interactionSlice := []float64{-0.1, -1, -0.1, -1, -0.1, -2, -2.6, -0.6, -3}
+
+	// 6. chaotic dynamics
+	interactionSlice := []float64{-1, -1.09, -1.52, 0, 0, -0.72, -0.3168, -0.9792, -3.5649, 0, -1.53, -0.7191, -1.5367, -0.6477, -0.4445, -1.27}
 
 	interaction := SetInteractionMatrix(interactionSlice, numSpecies)
 
@@ -42,8 +54,11 @@ func main() {
 	// // 1. original paper parameters
 	// rateSlice := []float64{0.25, -0.5, -0.5}
 
-	// 2. stable equilibrium & limit cycle
-	rateSlice := []float64{3, 4, 7.2}
+	// // 2. stable equilibrium & limit cycle & extinction
+	// rateSlice := []float64{3, 4, 7.2}
+
+	// 3. chaotic dynamics - 4 species
+	rateSlice := []float64{1, 0.72, 1.53, 1.27}
 
 	deathGrowth := SetRateMatrix(rateSlice)
 
@@ -67,7 +82,7 @@ func main() {
 
 	// writing data to csv file
 	fmt.Println("Writing data to csv file...")
-	WriteToCSV(timePoints, "output/data_limit_cycle.csv")
+	WriteToCSV(timePoints, "output/data_chaotic_dynamics.csv")
 	fmt.Println("Data written to csv file!")
 
 }
