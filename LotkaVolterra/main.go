58711d9acb86bd946c08100b2380f0d0591c78a4
KujoFulu <kujofulu@gmail.com>
2023-11-29 14:49:29 -0500

    Added py file for plotting

--- LotkaVolterra/main.go
+++ LotkaVolterra/main.go
@@ -16,42 +16,32 @@ func main() {
 	// population matrix
 	// 1. orginal paper
 	pop := []float64{50.0, 10.0, 5.0}
-
 	// // 2. stable equilibrium & limit cycle & extinction
 	// pop := []float64{0.1, 0.8, 0.3}
-
 	// // 3. chaotic dynamics - 4 species
 	// pop := []float64{0.1, 0.8, 0.3, 0.5}
 
+	// interaction matrix
 	// // method 1: randomly generate
 	// interaction := InitializeInteractionMatrix(numSpecies)
 	// method 2: set a matrix by hand
-
 	// 1. original paper parameters
 	interactionSlice := []float64{0, -0.04, -0.04, 0.04, 0, -0.02, 0.02, 0.04, 0}
-
 	// // 2. stable equilibrium
 	// interactionSlice := []float64{-2, -1, 0, 0, -1, -2, -2.6, -1.6, -3}
-
 	// // 3. limit cycle
 	// interactionSlice := []float64{-0.5, -1, 0, 0, -1, -2, -2.6, -1.6, -3}
-
 	// // 4. extinction of one species
 	// interactionSlice := []float64{-2, -1, -1, -1, -1, -2, -2.6, -1.6, -3}
-
 	// // 5. extinction of two species
 	// interactionSlice := []float64{-0.1, -1, -0.1, -1, -0.1, -2, -2.6, -0.6, -3}
-
 	// // 6. chaotic dynamics
 	// interactionSlice := []float64{-1, -1.09, -1.52, 0, 0, -0.72, -0.3168, -0.9792, -3.5649, 0, -1.53, -0.7191, -1.5367, -0.6477, -0.4445, -1.27}
 
-	interaction := SetInteractionMatrix(interactionSlice, numSpecies)
-
+	// deathGrowth matrix
 	// // method 1: randomly generate
 	// deathGrowth := IniRateMatrix(numSpecies)
-
 	// method 2: set a matrix by hand
-
 	// 1. original paper parameters
 	rateSlice := []float64{0.25, -0.5, -0.5}
 
@@ -61,11 +51,15 @@ func main() {
 	// // 3. chaotic dynamics - 4 species
 	// rateSlice := []float64{1, 0.72, 1.53, 1.27}
 
+	// set interaction and deathGrowth matrix: for simulation
+	interaction := SetInteractionMatrix(interactionSlice, numSpecies)
 	deathGrowth := SetRateMatrix(rateSlice)
 
-	// initialize number of generations and time interval
+	// initialize number of generations and time interval: for simulation
 	numGens := 50000
 	time := 0.002
+
+	// initialize canvas width and frequency: for drawing
 	canvasWidth := 500
 	frequency := 200
 
